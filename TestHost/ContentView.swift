639f8900a66e51f7f02bf1b88517df320985ec1e
musesum <musesum@gmail.com>
2026-05-23 15:30:02 +0800

    project update

---
+++ TestHost/ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+import MuHands
+
+struct ContentView: View {
+    var body: some View {
+        Text("MuHands test host")
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
